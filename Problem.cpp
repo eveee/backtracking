498068823bd3044c303fb503b2a0508c247586b8
Eva Ly Kok <evi.lyke@gmail.com>
2016-02-12 09:01:39 +0100

    Génération de problèmes
    rosenbrock, rastrigin, schwefel, ackley

--- Problem.cpp
+++ Problem.cpp
@@ -1,4 +1,5 @@
 #include "backtracking.h"
+#include "test_optimization.h"
 #include <cmath>
 
 Problem::Problem(int _id):fct_id(_id)
@@ -18,8 +19,16 @@ double Problem::evaluate(std::vector<double> x) const
 {
     switch (fct_id)
     {
-    case 0:
+    case 0: //fonction test
         return pow(-1,x[0])*(double)((int)x[0]%50);
+    case 1: //rosenbrock's valley
+        return *p04_f(_dimension, 1, &x[0]);
+    case 2: //rastrigin's function
+        return *p05_f(_dimension, 1, &x[0]);
+    case 3: //schwefel's function
+        return *p06_f(_dimension, 1, &x[0]);
+    case 4: //ackley's function
+       return  *p09_f(_dimension, 1, &x[0]);
     }
     return 0;
 }
